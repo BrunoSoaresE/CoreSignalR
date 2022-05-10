7267a951b0fccb9a3a62ad6326d7568512515763
bruno.soares <bruno.soares@netviewinformatica.com.br>
2022-05-09 21:16:22 -0300

    Adicionar arquivos de projeto.

---
+++ CoreSignalR/Hubs/PollHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CoreSignalR.Hubs
+{
+    public class PollHub : Hub
+    {
+        public async Task SendMessage(string user, string message, string myChannelId, string myChannelVal)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user, message, myChannelId,myChannelVal).ConfigureAwait(false);
+        }
+    }
+}
